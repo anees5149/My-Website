65d292eb0b54e329677a11496772e072950fc4e2
anees5149 <aneesfarooqui4@gmail.com>
2022-08-31 22:33:33 +0530

    commited

---
+++ ajax.js
@@ -0,0 +1,22 @@
+// console.log("Ajax")
+
+// let fetchBtn = document.getElementById('fetchBtn');
+// fetchBtn.addEventListener('click',  buttonClickHandler)
+
+// function buttonClickHandler() {
+//     console.log('you have clicked the fetchBtn');
+//     const xhr =new XMLHttpRequest();
+
+//     // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+//     // xhr.onload = function() {
+//     //     console.log(this.responseText)
+//     // }
+//     // xhr.send();
+//     xhr.open('POST', 'https://reqres.in/api/users', true);
+//     xhr.getResponseHeader('content-type', 'application/json');
+//     params = '{"name": "morpheus", "job": "leader"}';
+//     xhr.send(params);
+//     console.log('we are done')
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
